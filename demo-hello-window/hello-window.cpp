90b6f0cf24d0625b61f4864cf2631954e3c32ba5
Jonathan Dupuy <etu.jdupuy@gmail.com>
2018-03-24 16:33:42 +0100

    first commit

---
+++ demo-hello-window/hello-window.cpp
@@ -0,0 +1,43 @@
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include "imgui.h"
+
+#include <cstdio>
+#include <cstdlib>
+
+int main(int argc, char **argv)
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Hello OpenGL Window", NULL, NULL);
+	if (window == NULL)
+	{
+		printf("window creation failed!\n");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		printf("window creation failed!\n");
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	return 0;
+}
+
