be1a9293e6db4be02443653b2cbef4af4ff754ac
Jonathan Dupuy <etu.jdupuy@gmail.com>
2018-08-30 12:12:29 +0200

    added missing shader

---
+++ demo-isubd-terrain/shaders/cull.glsl
@@ -0,0 +1,81 @@
+/* dc_frustum - v1.0 - public domain frustum culling GLSL code
+	(Created by Jonathan Dupuy 2014.11.13)
+
+*/
+//////////////////////////////////////////////////////////////////////////////
+//
+// Frustum Culling API
+//
+
+bool dj_culltest(mat4 mvp, vec3 bmin, vec3 bmax);
+
+//
+//
+//// end header file /////////////////////////////////////////////////////
+
+
+// *****************************************************************************
+// Frustum Implementation
+
+struct dj_frustum {
+	vec4 planes[6];
+};
+
+/**
+ * Extract Frustum Planes from MVP Matrix
+ *
+ * Based on "Fast Extraction of Viewing Frustum Planes from the World-
+ * View-Projection Matrix", by Gil Gribb and Klaus Hartmann.
+ * This procedure computes the planes of the frustum and normalizes 
+ * them.
+ */
+void djc_load(out dj_frustum frustum, mat4 mvp)
+{
+	for (int i = 0; i < 3; ++i)
+	for (int j = 0; j < 2; ++j) {
+		frustum.planes[i*2+j].x = mvp[0][3] + (j == 0 ? mvp[0][i] : -mvp[0][i]);
+		frustum.planes[i*2+j].y = mvp[1][3] + (j == 0 ? mvp[1][i] : -mvp[1][i]);
+		frustum.planes[i*2+j].z = mvp[2][3] + (j == 0 ? mvp[2][i] : -mvp[2][i]);
+		frustum.planes[i*2+j].w = mvp[3][3] + (j == 0 ? mvp[3][i] : -mvp[3][i]);
+		frustum.planes[i*2+j]*= length(frustum.planes[i*2+j].xyz);
+	}
+}
+
+/**
+ * Negative Vertex of an AABB
+ *
+ * This procedure computes the negative vertex of an AABB
+ * given a normal.
+ * See the View Frustum Culling tutorial @ LightHouse3D.com
+ * http://www.lighthouse3d.com/tutorials/view-frustum-culling/geometric-approach-testing-boxes-ii/
+ */
+vec3 djc_nvertex(vec3 bmin, vec3 bmax, vec3 n)
+{
+	bvec3 b = greaterThan(n, vec3(0));
+	return mix(bmin, bmax, b);
+}
+
+/**
+ * Frustum-AABB Culling Test
+ *
+ * This procedure returns true if the AABB is either inside, or in
+ * intersection with the frustum, and false otherwise.
+ * The test is based on the View Frustum Culling tutorial @ LightHouse3D.com
+ * http://www.lighthouse3d.com/tutorials/view-frustum-culling/geometric-approach-testing-boxes-ii/
+ */
+bool dj_culltest(mat4 mvp, vec3 bmin, vec3 bmax)
+{
+	float a = 1.0;
+	dj_frustum f;
+
+	djc_load(f, mvp);
+	for (int i = 0; i < 6 && a >= 0.0; ++i) {
+		vec3 n = djc_nvertex(bmin, bmax, f.planes[i].xyz);
+
+		a = dot(vec4(n, 1.0), f.planes[i]);
+	}
+
+	return (a >= 0.0);
+}
+
+
